b709036af0b0f10e1a1074c001bfd21616fbfbfe
Ilja Patrushev <slowtnr@gmail.com>
2020-12-19 16:38:03 +0200

    Login Screen UI.

---
+++ FirebaseMessenger/Controllers/LoginRegister/RegisterViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterViewController.swift
+//  FirebaseMessenger
+//
+//  Created by Ilja Patrushev on 19.12.2020.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .link
+
+      
+    }
+    
+
+
+}
---
+++ FirebaseMessenger/Resources/Extentions.swift
@@ -0,0 +1,36 @@
+//
+//  Extentions.swift
+//  FirebaseMessenger
+//
+//  Created by Ilja Patrushev on 19.12.2020.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+        
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
