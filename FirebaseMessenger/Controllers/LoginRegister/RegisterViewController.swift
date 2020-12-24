c0038dffdebf00289e36b91f349ced709d6d87a4
Ilja Patrushev <slowtnr@gmail.com>
2020-12-24 19:37:42 +0200

    Add Searchbar, UI and other staff. gitignore Realm.

--- FirebaseMessenger/Controllers/LoginRegister/RegisterViewController.swift
+++ FirebaseMessenger/Controllers/LoginRegister/RegisterViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class RegisterViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
     
     private let scrollView : UIScrollView = {
         let scrollView = UIScrollView()
@@ -196,10 +199,16 @@ class RegisterViewController: UIViewController {
             return
         }
         
+        spinner.show(in: view)
+        
         // Firebase Sign Up
         
         DatabaseManager.shared.userExists(with: email, completion: { [weak self] exists in
             guard let strongSelf = self else {return}
+            
+            DispatchQueue.main.async {
+                strongSelf.spinner.dismiss()
+            }
             guard !exists else {
                 //user already exists
                 strongSelf.alertUserSignupError(message: "User already exists. Create new user with another email.")
--- FirebaseMessenger/Controllers/NewConversationViewController.swift
+++ FirebaseMessenger/Controllers/NewConversationViewController.swift
@@ -6,24 +6,61 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class NewConversationViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for Users."
+        return searchBar
+    }()
+    
+    private let tableVIew: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    private let noResultLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Results"
+        label.textAlignment = .center
+        label.textColor = .green
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+        
+        // TODO: Do something
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        view.backgroundColor = .white
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(dismissSelf))
+        
+        searchBar.becomeFirstResponder()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func dismissSelf() {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
+
+}
+
+
+extension NewConversationViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
 }
