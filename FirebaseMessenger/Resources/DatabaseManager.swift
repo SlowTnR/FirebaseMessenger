e5e63bf7bcdf9a862ea6696fff79fd75cca68d06
Ilja Patrushev <slowtnr@gmail.com>
2021-01-07 23:16:13 +0200

    Delete chat.

--- FirebaseMessenger/Resources/DatabaseManager.swift
+++ FirebaseMessenger/Resources/DatabaseManager.swift
@@ -37,7 +37,8 @@ extension DatabaseManager {
         
         database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             
-            guard  snapshot.value as? String != nil else {
+            //guard  snapshot.value as? String != nil else {
+            guard  snapshot.exists()  else {
                 completion(false)
                 return
                 
@@ -579,6 +580,46 @@ extension DatabaseManager {
             })
         })
     }
+    
+    public func deleteConversation(conversationId: String, completion: @escaping (Bool) -> Void){
+        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+            return
+        }
+        
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+        
+        print("Deleteng conversation with id: \(conversationId)")
+        
+        // get all conversations for current user
+        // delete conversation in collection with target id
+        // resete those conversations for user and databes
+        let ref = database.child("\(safeEmail)/conversations")
+        
+        ref.observeSingleEvent(of: .value, with: { snapshot in
+            if var conversations = snapshot.value as? [[String: Any]]{
+                var positionToRemove = 0
+                for conversation in conversations {
+                    if let id = conversation["id"] as? String,
+                       id == conversationId {
+                        print("found conversation to delete")
+                        break
+                    }
+                    positionToRemove += 1
+                }
+                conversations.remove(at: positionToRemove)
+                ref.setValue(conversations, withCompletionBlock: {error, _ in
+                    guard error == nil else {
+                        completion(false)
+                        print("failed to delete conversation")
+                        return
+                    }
+                    print("deleted conversation")
+                    completion(true)
+                })
+            }
+        })
+    }
+    
 }
 
 
